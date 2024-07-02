0239f4af6a108ad7afd0efa221e7a212e8642ba7
Igor Sheko <i@irbisadm.dev>
2024-07-02 10:08:34 +0700

    chore: version 0.0.1

--- src/codec/av1/av1-info.ts
+++ src/codec/av1/av1-info.ts
@@ -3,45 +3,47 @@ import {ColourPrimaries, MatrixCoefficients, TransferCharacteristics, VideoFullR
 import {Av1BitDepth, Av1ChromaSamplePosition, Av1Level, Av1Profile, Av1Tier} from "./enums";
 
 //https://aomediacodec.github.io/av1-spec/av1-spec.pdf#page=2
-export class Av1Info extends CodecInfo{
+export class Av1Info extends CodecInfo {
 
   codecName = 'av1';
 
-  private _profile:Av1Profile = Av1Profile.UNSET;
-  private _level:Av1Level = Av1Level.UNSET;
-  private _tier:Av1Tier = Av1Tier.UNSET;
-  private _bitDepth:Av1BitDepth = Av1BitDepth.UNSET;
+  private _profile: Av1Profile = Av1Profile.UNSET;
+  private _level: Av1Level = Av1Level.UNSET;
+  private _tier: Av1Tier = Av1Tier.UNSET;
+  private _bitDepth: Av1BitDepth = Av1BitDepth.UNSET;
   private _monochrome: boolean = false;
   private _chromaSubsamplingX: boolean = false;
   private _chromaSubsamplingY: boolean = false;
   private _chromaSamplePosition: Av1ChromaSamplePosition = Av1ChromaSamplePosition.UNKNOWN;
-  private _colorPrimaries:ColourPrimaries = ColourPrimaries.UNSPECIFIED
-  private _transferCharacteristics:TransferCharacteristics = TransferCharacteristics.UNSPECIFIED
-  private _matrixCoefficients:MatrixCoefficients = MatrixCoefficients.UNSPECIFIED
-  private _videoFullRangeFlag:VideoFullRangeFlag = VideoFullRangeFlag.LEGAL
+  private _colorPrimaries: ColourPrimaries = ColourPrimaries.UNSPECIFIED
+  private _transferCharacteristics: TransferCharacteristics = TransferCharacteristics.UNSPECIFIED
+  private _matrixCoefficients: MatrixCoefficients = MatrixCoefficients.UNSPECIFIED
+  private _videoFullRangeFlag: VideoFullRangeFlag = VideoFullRangeFlag.LEGAL
 
-  constructor() { super(); }
+  constructor() {
+    super();
+  }
 
   get chromaSubsampling(): string {
-    return `${this._chromaSubsamplingX ? 1 : 0}${this._chromaSubsamplingY ? 1 : 0}${this._chromaSubsamplingX&&this._chromaSubsamplingY ? this._chromaSamplePosition : '0'}`;
+    return `${this._chromaSubsamplingX ? 1 : 0}${this._chromaSubsamplingY ? 1 : 0}${this._chromaSubsamplingX && this._chromaSubsamplingY ? this._chromaSamplePosition : '0'}`;
   }
 
   set chromaSubsampling(chromaSubsampling: string) {
     this._chromaSubsamplingX = chromaSubsampling[0] === '1';
     this._chromaSubsamplingY = chromaSubsampling[1] === '1';
-    if(this._chromaSubsamplingX && this._chromaSubsamplingY){
-     this._chromaSamplePosition = parseInt(chromaSubsampling[2]) as Av1ChromaSamplePosition;
+    if (this._chromaSubsamplingX && this._chromaSubsamplingY) {
+      this._chromaSamplePosition = parseInt(chromaSubsampling[2]) as Av1ChromaSamplePosition;
     }
   }
 
-  toString():string{
+  toString(): string {
     const isoParts = [];
     //<sample entry 4CC>.<profile>.<level><tier>.<bitDepth>.<monochrome>.<chromaSubsampling>.
     // <colorPrimaries>.<transferCharacteristics>.<matrixCoefficients>.<videoFullRangeFlag>
     isoParts.push(this.chromaSubsampling);
     isoParts.push(this._monochrome ? '1' : '0');
     isoParts.push(this._bitDepth.toString());
-    isoParts.push(this._level.toString()+this._tier.toString());
+    isoParts.push(this._level.toString() + this._tier.toString());
     isoParts.push(this._profile);
     isoParts.push('av01');
     return isoParts
--- src/codec/vpx/vp8-info.spec.ts
+++ src/codec/vpx/vp8-info.spec.ts
@@ -1,7 +1,6 @@
 import {ColourPrimaries, VideoFullRangeFlag} from "../iso-23001-8_2016";
 import {Vp8Info} from "./vp8-info";
 import {VpxBitDepth, VpxChromaSubsampling, VpxLevel, VpxProfile} from "./enums";
-import {codecInfoFactory, codecInfoFromBox} from "../../index";
 
 describe('VP8 codecs tests', () => {
   it('without settings', () => {
@@ -10,40 +9,32 @@ describe('VP8 codecs tests', () => {
     expect(codecInfo.level).toBe(VpxLevel.UNDEFINED);
     expect(codecInfo.chromaSubsampling).toBe(VpxChromaSubsampling.CS_420_COLOCATED_0_0);
     expect(codecInfo.colourPrimaries).toBe(ColourPrimaries.BT_709);
-    expect(''+codecInfo).toBe('vp08');
+    expect('' + codecInfo).toBe('vp08');
   })
   it('vp0 full box', () => {
     const codecInfo = new Vp8Info();
     codecInfo.videoFullRangeFlag = VideoFullRangeFlag.FULL;
     codecInfo.bitDepth = VpxBitDepth.BIT_DEPTH_8;
-    expect(''+codecInfo).toBe('vp08.00.00.08.01.01.01.01.01');
+    expect('' + codecInfo).toBe('vp08.00.00.08.01.01.01.01.01');
   })
   it('set profile 0', () => {
     const codecInfo = new Vp8Info();
     codecInfo.profile = VpxProfile.PROFILE_0;
-    expect(''+codecInfo).toBe('vp08');
+    expect('' + codecInfo).toBe('vp08');
   })
   it('set profiles 1-2-3', () => {
     const codecInfo = new Vp8Info();
-    expect(()=>{codecInfo.profile = VpxProfile.PROFILE_1}).toThrow('VP8 only supports a profile value of 0.');
+    expect(() => {
+      codecInfo.profile = VpxProfile.PROFILE_1
+    }).toThrow('VP8 only supports a profile value of 0.');
     expect(codecInfo.profile).toBe(VpxProfile.PROFILE_0);
-    expect(()=>{codecInfo.profile = VpxProfile.PROFILE_2}).toThrow('VP8 only supports a profile value of 0.');
+    expect(() => {
+      codecInfo.profile = VpxProfile.PROFILE_2
+    }).toThrow('VP8 only supports a profile value of 0.');
     expect(codecInfo.profile).toBe(VpxProfile.PROFILE_0);
-    expect(()=>{codecInfo.profile = VpxProfile.PROFILE_3}).toThrow('VP8 only supports a profile value of 0.');
+    expect(() => {
+      codecInfo.profile = VpxProfile.PROFILE_3
+    }).toThrow('VP8 only supports a profile value of 0.');
     expect(codecInfo.profile).toBe(VpxProfile.PROFILE_0);
-  })
-  it('speedtest',()=>{
-    const start = performance.now();
-    for(let i=0;i<1000000;i++){
-      codecInfoFactory('vp09.02.10.10.01.09.16.09.01');
-    }
-    const end = performance.now();
-    const start2 = performance.now();
-    for(let i=0;i<1000000;i++) {
-      codecInfoFromBox('vp09.02.10.10.01.09.16.09.01');
-    }
-    const end2 = performance.now();
-    console.log(`Fancy class ${end-start}`);
-    console.log(`Simple function ${end2-start2}`);
   })
 })
